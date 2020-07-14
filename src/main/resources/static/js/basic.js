de583db0d1c13ffc6c3a4b95c8258946141f092e
Gyarmathy Eszter <gyarmathyeszter.est@gmail.com>
2020-07-14 13:27:13 +0200

    basic tableganarator, beginner pointgenerator

--- src/main/resources/static/js/basic.js
+++ src/main/resources/static/js/basic.js
@@ -1,14 +1,20 @@
-{
+function tablegenerator() {
 
-    function tablegenerator() {
-            var htmlString ="<tbody><tr>";
-
-            for (i = 0; i < 10; i++) {
-                htmlString + "<td></td>";
-            }
-    			htmlString + "</tbody></tr>";
-            document.getElementById("basictable").innerHTML = htmlString;
-
+    for (i = 0; i < 35; i++) {
+        var rowDiv = document.createElement('div');
+        rowDiv.className = 'rowdiw';
+        document.getElementById('tablediv').appendChild(rowDiv);
+        for (j = 0; j < 35; j++) {
+            var cellDiv = document.createElement('div');
+            cellDiv.className = 'celldiv';
+            cellDiv.setAttribute("onclick", "pointgenerator()");
+            rowDiv.appendChild(cellDiv);
         }
+    }
+}
+
+function pointgenerator() {
+
+    document.write = "cica";
 
 }
