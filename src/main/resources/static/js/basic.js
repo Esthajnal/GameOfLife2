398ba188ae0781055d9993143eb1d075a6e6b7dd
Gyarmathy Eszter <gyarmathyeszter.est@gmail.com>
2020-07-15 10:32:06 +0200

    point exists.

--- src/main/resources/static/js/basic.js
+++ src/main/resources/static/js/basic.js
@@ -7,14 +7,14 @@ function tablegenerator() {
         for (j = 0; j < 35; j++) {
             var cellDiv = document.createElement('div');
             cellDiv.className = 'celldiv';
-            cellDiv.setAttribute("onclick", "pointgenerator()");
+            cellDiv.setAttribute("onclick", "pointgenerator(event)");
             rowDiv.appendChild(cellDiv);
         }
     }
 }
 
-function pointgenerator() {
-
-    document.write = "cica";
-
+function pointgenerator(event) {
+    var point = document.createElement('div');
+    point.className = 'point';
+    event.currentTarget.appendChild(point);
 }
