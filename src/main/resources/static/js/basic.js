494b4905399dcd3b0978af5c041194c775a36ecd
Gyarmathy Eszter <gyarmathyeszter.est@gmail.com>
2020-07-16 20:28:56 +0200

    without solution

--- src/main/resources/static/js/basic.js
+++ src/main/resources/static/js/basic.js
@@ -1,20 +1,46 @@
 function tablegenerator() {
+    if (document.getElementById('tablediv').hasChildNodes()) {
+        var goalDiv = document.getElementById('goaldiv');
+        goalDiv.innerHTML = 'Létezik tábla';
+    } else {
 
-    for (i = 0; i < 35; i++) {
-        var rowDiv = document.createElement('div');
-        rowDiv.className = 'rowdiw';
-        document.getElementById('tablediv').appendChild(rowDiv);
-        for (j = 0; j < 35; j++) {
-            var cellDiv = document.createElement('div');
-            cellDiv.className = 'celldiv';
-            cellDiv.setAttribute("onclick", "pointgenerator(event)");
-            rowDiv.appendChild(cellDiv);
+        for (i = 0; i < 10; i++) {
+            var rowDiv = document.createElement('div');
+            rowDiv.className = 'rowdiv';
+            document.getElementById('tablediv').appendChild(rowDiv);
+            for (j = 0; j < 10; j++) {
+                var cellDiv = document.createElement('div');
+                cellDiv.className = 'celldiv';
+                cellDiv.setAttribute('onclick', 'pointgenerator(event)');
+                rowDiv.appendChild(cellDiv);
+            }
         }
     }
 }
 
 function pointgenerator(event) {
-    var point = document.createElement('div');
-    point.className = 'point';
-    event.currentTarget.appendChild(point);
+    var div = event.currentTarget;
+
+    if (div.hasChildNodes()) {
+        var goalDiv = document.getElementById('goaldiv');
+        goalDiv.innerHTML = 'Már jelölted';
+
+    } else {
+        var point = document.createElement('span');
+        point.className = 'point';
+        div.appendChild(point);
+    }
+}
+
+function roundgenerator() {
+
+    var divList1 = document.getElementsByClassName('rowdiv');
+    var divList2 = document.getElementsByClassName('celldiv');
+    var cellArray = new Array();
+
+    for(i=0;i<divList2.length;i++){
+            for(j=0; i<divList1.length;j++){
+                cellArray
+            }
+    }
 }
